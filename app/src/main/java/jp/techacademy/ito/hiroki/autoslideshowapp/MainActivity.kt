3591209b57e407ed38cded01de2234b8ccdfbe71
Hitoki <diavolos1230@gmail.com>
2019-11-22 18:28:26 +0900

    少し進んだ！

--- app/src/main/java/jp/techacademy/ito/hiroki/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/ito/hiroki/autoslideshowapp/MainActivity.kt
@@ -1,19 +1,22 @@
 package jp.techacademy.ito.hiroki.autoslideshowapp
 
 import android.Manifest
+import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.pm.PackageManager
 import android.os.Build
 //import android.util.Log
-import android.view.View //追加
+import android.view.View
 import android.provider.MediaStore
 import android.content.ContentUris
 import kotlinx.android.synthetic.main.activity_main.*
 
 private val PERMISSIONS_REQUEST_CODE = 100 //←これ必要なの？　8.3
 
-class MainActivity : AppCompatActivity, View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+        var cursor: Cursor? = null //メンバ変数(ﾟдﾟ)！
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +30,10 @@ class MainActivity : AppCompatActivity, View.OnClickListener {
                 getContentsInfo()
             } else {
                 // 許可されていないので許可ダイアログを表示する
-                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSIONS_REQUEST_CODE
+                )
             }
             // Android 5系以下の場合
         } else {
@@ -36,10 +42,14 @@ class MainActivity : AppCompatActivity, View.OnClickListener {
 
         go_button.setOnClickListener(this)
         back_button.setOnClickListener(this)
-        pause_button.setOnClickListener(this)
+        playpause_button.setOnClickListener(this)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             PERMISSIONS_REQUEST_CODE ->
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -48,35 +58,31 @@ class MainActivity : AppCompatActivity, View.OnClickListener {
         }
     }
 
-    private fun getContentsInfo() {
+    public fun getContentsInfo() {
         // 画像の情報を取得する
         val resolver = contentResolver
-        val cursor = resolver.query(
+        this.cursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI, // データの種類
             null, // 項目(null = 全項目)
             null, // フィルタ条件(null = フィルタなし)
             null, // フィルタ用パラメータ
             null // ソート (null ソートなし)
         )
-
         if (cursor!!.moveToFirst()) {
-            do {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
-
-                imageView.setImageURI(imageUri)
-            } while (cursor.moveToNext())
-        }
-        cursor.close()
+            /*do {*/
+            // indexからIDを取得し、そのIDから画像のURIを取得する
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri =
+                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            imageView.setImageURI(imageUri)
+        } //while (cursor.moveToNext())
+        //cursor!!.close()
     }
 
     override fun onClick(v: View) {
         if (v.id == R.id.go_button) {
-            // ここに表示view追加
-        } else if (v.id == R.id.back_button) {
-            // ここに表示view追加
+            cursor!!.moveToNext()
         }
     }
 
